a50a084984e8f3b73871c8ce73d8c1899dd71d1f
LuisC0rd <luis.gcsilva3@senacsp.edu.br>
2025-02-06 15:27:41 -0300

    chore: customize nav bar options

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,36 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function TabsLayout() {
+function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#FFD33D",
+        headerStyle: { backgroundColor: "#25292E" },
+        headerShadowVisible: false,
+        headerTintColor: "#FFF",
+        tabBarStyle: {
+          backgroundColor: "#25292E",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          // arrow function
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen name="about" options={{ title: "About" }} />
     </Tabs>
   );
 }
+
+export default TabsLayout;
