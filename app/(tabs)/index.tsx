48e5fa86caf0a8387d4f22c76179efec857b5f00
LuisC0rd <luis.gcsilva3@senacsp.edu.br>
2025-02-06 17:32:03 -0300

    Chore: use expo-image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images-backgroud-Image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View>
+        <Image source={PlaceholderImage} />
+      </View>
       <Text style={styles.text}>HOME</Text>
       <Link href="/about" style={styles.button}>
         Go to About screen
